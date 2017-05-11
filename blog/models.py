533d09b10405358ace4c877686ca1bd8c3a259ef
JJ <jj.spetseris@yahoo.com>
2017-05-11 09:26:27 -0500

    initial blog app commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Blog(models.Model):
+  name = models.CharField(max_length=50)
+  slug = models.SlugField(max_length=50, unique=True)
+class Post(models.Model):
+  title = models.CharField(max_length=50)
+  subtitle = models.CharField(max_length=140,
+                              blank=True, null=True)
+  slug = models.SlugField(max_length=50, unique=True)
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  blog = models.ForeignKey(Blog)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL)
